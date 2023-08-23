68a4d6ba75d845944a8c65b3ce19e954fc4266ce
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2023-08-23 22:18:51 +0200

    Publish: '0b89e01'

--- EK/cs_after_load.js
+++ EK/cs_after_load.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
-    
+    var sets = document.getElementById("sets");
+	sets.innerHTML += '<a href="https://docs.google.com/spreadsheets/d/1-xSxcGzBTExNIAE_77jNfJosd-4D5852hq_IKkJpeWs/edit?usp=sharing">EK AI Bait spreadsheet</a>'
 });
